cdcfa83c8759bcdb6e5ed9bf888b3b99c88d258e
SvitlanaSavchenko <134533907+SvitlanaSavchenko@users.noreply.github.com>
2023-10-16 09:16:04 +0200

    Update task-1.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,28 @@
-// Bикористовується метод querySelectorAll для вибору всіх елементів li.item в списку ul#categories.. далі він проходить цей список за допомогою методу forEach і для кожного елемента знаходить назву категорії і кількість елементів в цій категорії.. результат виводиться в консолі.
+// Знаходиться елемент ul з id "categories" за допомогою document.querySelector("#categories") і зберігається у змінну categoriesList.
 
-const categoriesList = document.querySelectorAll("ul#categories .item");
+const categoriesList = document.querySelector("#categories");
 
-console.log(`Number of categories: ${categoriesList.length}`);
+// Виводиться в консоль кількість категорій, використовується властивість .children для отримання дочірніх елементів ul#categories та їх кількість.
 
-categoriesList.forEach((category) => {
+console.log(`Number of categories: ${categoriesList.children.length}`);
+
+// Знаходиться всі елементи li з класом "item" всередині елемента ul#categories. Використовується categoriesList.querySelectorAll(".item") і зберігається у псевдомасив categoryItems.
+
+const categoryItems = categoriesList.querySelectorAll(".item");
+
+// Використовується метод forEach, перебираються всі категорії
+
+categoryItems.forEach((category) => {
+  // Знаходиться заголовок категорії (тег <h2>) в поточній категорії за допомогою category.querySelector("h2").
+
   const categoryName = category.querySelector("h2").textContent;
-  const categoryItems = category.querySelectorAll("ul li");
+
+  // Знаходяться всі елементи <li> всередині поточної категорії за допомогою category.querySelectorAll("li").
+
+  const categoryItemsCount = category.querySelectorAll("li").length;
+
+  // Виводиться в консоль текст загодовка та к-сть елементів
+
   console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryItems.length}`);
+  console.log(`Elements: ${categoryItemsCount}`);
 });
